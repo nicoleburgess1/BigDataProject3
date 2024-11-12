38b373acda9cea5aa46512ee464de390e26f5c6a
nicoleburgess1 <nburgess1113@gmail.com>
2024-11-12 10:39:29 -0500

    task1

--- src/main/Scala/Task1.scala
+++ src/main/Scala/Task1.scala
@@ -2,16 +2,18 @@ import org.apache.spark.sql.SparkSession
 /*
 Task 2.1) Filter out (drop) the Purchases from P with a total purchase amount above $600.
 Store the result as T1.
+part-00000-f17e3133-8bd4-45b3-8e3c-5c7343722eb6-c000.csv
  */
 import org.apache.spark.sql.SparkSession
 
 object Task1 {
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder()
+    /*val spark = SparkSession.builder()
       .appName("DataFrameExample")
       .master("local[*]")
       .getOrCreate()
+    spark.catalog.clearCache()
 
     import spark.implicits._
 
@@ -20,26 +22,28 @@ object Task1 {
 
     df.show()
 
+    spark.stop()*/
+
+    val spark = SparkSession.builder()
+      .appName("T1")
+      .config("spark.master", "local")
+      .getOrCreate()
+    spark.catalog.clearCache()
+    val df = spark.read.csv("Purchases.csv")
+    df.show()
+
+    // Register DataFrame as a SQL temporary view
+    df.createOrReplaceTempView("purchases")
+
+    // SQL query
+    val sqlDF = spark.sql("SELECT * FROM purchases WHERE _c1 > 600")
+    sqlDF.show()
+    sqlDF.coalesce(1).write.csv("T1Result.csv")
+
+    // Save result
+    //sqlDF.write.json("path/to/output/json")
+
+    // Stop the Spark session
     spark.stop()
   }
-  /*val spark = SparkSession.builder()
-    .appName("T1")
-    .config("spark.master", "local")
-    .getOrCreate()
-  val df = spark.read.csv("Purchases.csv")
-  df.show()
-
-  // Register DataFrame as a SQL temporary view
-  df.createOrReplaceTempView("people")
-
-  // SQL query
-  val sqlDF = spark.sql("SELECT name, age FROM people WHERE age > 24")
-  sqlDF.show()
-
-  // Save result
-  sqlDF.write.json("path/to/output/json")
-
-  // Stop the Spark session
-  spark.stop()*/
-
 }
