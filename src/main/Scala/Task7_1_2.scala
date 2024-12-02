1eb1b0d89bcf2d3b2ae1925174bf4f5161076478
aalapsley <114630770+aalapsley1@users.noreply.github.com>
2024-12-01 22:43:02 -0500

    Update Task7_1_2.scala

--- src/main/Scala/Task7_1_2.scala
+++ src/main/Scala/Task7_1_2.scala
@@ -1,5 +1,5 @@
 import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
-import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.evaluation.{BinaryClassificationMetrics, MulticlassMetrics}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.SparkSession
@@ -7,7 +7,7 @@ import org.apache.spark.sql.SparkSession
 object Task7_1_2 {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
-      .appName("T3")
+      .appName("T7.1.2")
       .config("spark.master", "local")
       .getOrCreate()
     val sc = spark.sparkContext
@@ -37,11 +37,40 @@ object Task7_1_2 {
 
 
 
-    model.save(sc, "target/tmp/scalaLogisticRegressionWithLBFGSModel")
-    val sameModel = LogisticRegressionModel.load(sc,
-      "target/tmp/scalaLogisticRegressionWithLBFGSModel")
+    val scoreAndLabels = test.map { point =>
+      val score = model.predict(point.features)
+      (score, point.label)
+    }
 
+    val metrics_2 = new BinaryClassificationMetrics(scoreAndLabels)
 
+    val f1Score = metrics_2.fMeasureByThreshold
+    f1Score.collect.foreach { case (t, f) =>
+      println(s"Threshold: $t, F-score: $f, Beta = 1")
+    }
+
+    val auROC = metrics_2.areaUnderROC()
+
+    println(s"Area under ROC = $auROC")
+
+    val auPRC = metrics_2.areaUnderPR
+    println(s"Area under precision-recall curve = $auPRC")
+
+    val beta = 0.5
+    val fScore = metrics_2.fMeasureByThreshold(beta)
+    fScore.collect.foreach { case (t, f) =>
+      println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+    }
+
+    val precision = metrics_2.precisionByThreshold
+    precision.collect.foreach { case (t, p) =>
+      println(s"Threshold: $t, Precision: $p")
+
+      model.save(sc, "target/tmp/scalaLogisticRegressionWithLBFGSModel")
+      val sameModel = LogisticRegressionModel.load(sc,
+        "target/tmp/scalaLogisticRegressionWithLBFGSModel")
+
+}
   }
 
 
